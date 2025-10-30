17294eaf98364eb13a978dac98f8040ed4b28bc1
Mirzohiddev006 <ibrohimjonovm2006@gmail.com>
2025-10-30 18:03:46 +0500

    single-movie-added

--- web/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./login";
-export * from "./register";
-export * from "./movies";
-export * from "./single-movie";
-export * from "./new-movie";
--- web/src/utils/pagination-logics.ts
+++
@@ -1,7 +0,0 @@
-import _ from "lodash";
-import type { Movie } from "~/types";
-
-export const paginate = (movies: Movie[], currentPage: number, pageSize: number) => {
-  const startIdx = (currentPage - 1) * pageSize;
-  return _(movies).slice(startIdx).take(pageSize).value();
-};
